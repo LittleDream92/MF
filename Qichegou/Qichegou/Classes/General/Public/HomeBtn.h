b5a51f82a85142bd779fd69321c7a56c084da86b
Your Name <LittleDream92>
2016-09-21 14:33:50 +0800

    ran 2

---
+++ Qichegou/Qichegou/Classes/General/Public/HomeBtn.h
@@ -0,0 +1,13 @@
+//
+//  HomeBtn.h
+//  QiChegou
+//
+//  Created by Meng Fan on 16/9/18.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeBtn : UIButton
+
+@end
---
+++ Qichegou/Qichegou/Classes/General/Public/HomeBtn.m
@@ -0,0 +1,35 @@
+//
+//  HomeBtn.m
+//  QiChegou
+//
+//  Created by Meng Fan on 16/9/18.
+//  Copyright © 2016年 Meng Fan. All rights reserved.
+//
+
+#import "HomeBtn.h"
+
+@implementation HomeBtn
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+}
+
+-(CGRect)imageRectForContentRect:(CGRect)contentRect {
+    
+    return CGRectMake(contentRect.origin.x, 0, contentRect.size.width, contentRect.size.height-20);
+}
+
+-(CGRect)titleRectForContentRect:(CGRect)contentRect {
+    return CGRectMake(contentRect.origin.x, contentRect.size.height-20, contentRect.size.width, 20);
+}
+
+//取消高亮状态
+-(void)setHighlighted:(BOOL)highlighted {
+    
+}
+
+
+@end
